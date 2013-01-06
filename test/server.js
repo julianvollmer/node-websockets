95b774021a0441675e2cf259ffb4be379ed9896d
bodokaiser <bodo.kaiser@enabre.com>
2013-01-06 17:42:09 +0100

    refactored frame

--- test/server.js
+++ test/server.js
@@ -14,7 +14,7 @@ httpServer.on('request', function(req, res) {
 webSocketServer.on('upgraded', function(socket, head) {
     socket.on('data', function(buff) {
         var frame = new Frame(buff);
-        console.log(frame.getPayloadLength());
+        console.log(frame.unmaskPayload().toString());
     }); 
 });
 
