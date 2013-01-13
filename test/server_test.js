50048f8d6b7b6487e129ead0549da5fa7e42d9f1
bodokaiser <bodo.kaiser@enabre.com>
2013-01-13 12:31:35 +0100

    added send method to WebSocketServer

--- test/server_test.js
+++ test/server_test.js
@@ -3,16 +3,29 @@ var http = require('http');
 var WebSocketServer = require('../lib/server');
 
 var httpServer = http.createServer();
-var webSocketServer = new WebSocketServer('ws://localhost');
+var wss = new WebSocketServer('ws://localhost');
 
 httpServer.on('request', function(req, res) {
     res.setHeader('Content-Type', 'text/plain');
     res.end('hello world');
 });
 
-webSocketServer.onmessage = function(message) {
-    console.log(message);
+wss.onopen = function() {
+    console.log('connection opened');  
+
+    wss.send('hello');
 };
 
+wss.onmessage = function(message) {
+    console.log('receiving message: ', message);
+    
+    wss.send(message);
+};
+
+wss.onclose = function() {
+    console.log('connection closed');  
+};
+
+wss.listen(httpServer);
+
 httpServer.listen(3000);
-webSocketServer.listen(httpServer);
