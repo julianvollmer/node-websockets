b357f92a7acbd46b3ed2073418dded79d560419f
Bodo Kaiser <bodo.kaiser@enabre.com>
2013-02-12 03:08:37 -0500

    used should.have.property instead of prop.should at url parsing

--- test/base.js
+++ test/base.js
@@ -16,26 +16,26 @@ describe('WebSocketBase', function() {
             var wsb = new WebSocketBase();
             
             wsb.url.should.be.a('object');
-            wsb.url.slashes.should.be.true;
-            wsb.url.protocol.should.equal('ws:');
-            wsb.url.hostname.should.equal('localhost');
-            wsb.url.href.should.equal('ws://localhost:3000');
-            wsb.url.host.should.equal('localhost:3000');
-            wsb.url.port.should.equal('3000');
-            wsb.url.path.should.equal('/');
+            wsb.url.should.have.property('slashes', true);
+            wsb.url.should.have.property('protocol', 'ws:');
+            wsb.url.should.have.property('hostname', 'localhost');
+            wsb.url.should.have.property('href', 'ws://localhost:3000');
+            wsb.url.should.have.property('host', 'localhost:3000');
+            wsb.url.should.have.property('port', '3000');
+            wsb.url.should.have.property('path', '/');
         });
         
         it('should use the url defined in options if provided', function() {
             var wsb = new WebSocketBase({ url: "ws://sockets.org:5000/index" });
             
             wsb.url.should.be.a('object');
-            wsb.url.slashes.should.be, true;
-            wsb.url.protocol.should.equal('ws:');
-            wsb.url.hostname.should.equal('sockets.org');
-            wsb.url.href.should.equal('ws://sockets.org:5000/index');
-            wsb.url.host.should.equal('sockets.org:5000');
-            wsb.url.port.should.equal('5000');
-            wsb.url.path.should.equal('/index');            
+            wsb.url.should.have.property('slashes', true);
+            wsb.url.should.have.property('protocol', 'ws:');
+            wsb.url.should.have.property('hostname', 'sockets.org');
+            wsb.url.should.have.property('href', 'ws://sockets.org:5000/index');
+            wsb.url.should.have.property('host', 'sockets.org:5000');
+            wsb.url.should.have.property('path', '/index');
+            wsb.url.should.have.property('port', '5000'); 
         });
     });
     
