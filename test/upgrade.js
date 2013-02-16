641b2baaafa635180801d23176a617899d9c74f4
bodokaiser <bodo.kaiser@enabre.com>
2013-02-16 12:38:29 +0100

    fixed and extended upgrade test and algorithm

--- test/upgrade.js
+++ test/upgrade.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var assert = require('assert');
+var should = require('should');
 
 var WebSocketUpgrade = require('../lib/upgrade');
 
@@ -7,42 +8,70 @@ var WebSocketUpgrade = require('../lib/upgrade');
 describe('WebSocketUpgrade', function() {
    
     describe('#createUpgradeRequest()', function() {
-        
+
+        var server;
+
+        before(function() {
+            server  = http.createServer().listen(3000);
+        });
+       
         it('should send a ws upgrade request to a server', function(done) {
-            var server = http.createServer();
-            
-            server.on('upgrade', function(req, socket) {
-                var headers = req.headers;
-                
-                assert.equal(headers['upgrade'], 'websocket');
-                assert.equal(headers['connection'], 'upgrade');
-                //assert.equal(headers['sec-websocket-key'], 'string');
-                assert.equal(headers['sec-websocket-version'], 13);
-                
+            server.once('upgrade', function(req, socket) {
+                req.headers.should.have.property('upgrade', 'websocket');
+                req.headers.should.have.property('connection', 'upgrade');
+                req.headers.should.have.property('sec-websocket-key');
+                req.headers.should.have.property('sec-websocket-version', '13');
+
                 done();
             });
-            
-            server.listen(3000);
             
             WebSocketUpgrade.createUpgradeRequest('ws://localhost:3000');
         });
         
+        after(function() {
+            // close the server
+            server.close();
+        });
+
     });
     
     describe('#handleUpgradeRequest()', function() {
-    
-        it('should read the ws upgrade request and send back a switching protocol response', function() {
-            var server = http.createServer();
-            
-            server.on('upgrade', function(req, socket) {
+
+        var server;
+
+        before(function() {
+            server = http.createServer().listen(3000);
+        });
+                        
+        it('should read the ws upgrade request and call the optional callback', function(done) {
+            server.once('upgrade', function(req, socket) {
                 WebSocketUpgrade.handleUpgradeRequest(req, socket, function(socket) {
                     done();
                 });
             });
             
-            server.listen(3000);
-            
             WebSocketUpgrade.createUpgradeRequest('ws://localhost:3000');
+        });
+
+        it('should send back a valid http websocket upgrade response', function(done) {
+            server.once('upgrade', function(req, socket) {
+                WebSocketUpgrade.handleUpgradeRequest(req, socket);
+            });
+            
+            WebSocketUpgrade.createUpgradeRequest('ws://localhost:3000', function(res, socket) {
+                res.statusCode.should.equal(101);
+                
+                res.headers.should.have.property('upgrade', 'websocket');
+                res.headers.should.have.property('connection', 'upgrade');
+                res.headers.should.have.property('sec-websocket-accept');
+
+                done();     
+            });
+        });
+        
+        after(function() {
+            // close the server
+            server.close();
         });
 
     });
