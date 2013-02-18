3ea954b21e2d251f1b97f69148c1d25c6b87fde9
bodokaiser <bodo.kaiser@enabre.com>
2013-02-18 14:19:22 +0100

    improved server tests

--- test/server.js
+++ test/server.js
@@ -23,6 +23,32 @@ describe('WebSocketServer', function() {
         server.close();
     });
 
+    it('should inherit methods from WebSocketBase', function() {
+        var wss = new WebSocketServer();
+        wss.should.have.property('on');
+        wss.should.have.property('emit');
+        wss.should.have.property('send');
+        wss.should.have.property('ping');
+        wss.should.have.property('close');
+        wss.should.have.property('hasExtension');
+        wss.should.have.property('addExtension');
+        wss.should.have.property('removeExtension');
+        wss.should.have.property('assignSocket');
+    });
+
+    describe('#constructor()', function() {
+        it('should use parse the url if provided', function() {
+            var wss = new WebSocketServer({ url: ressource });
+            wss.should.have.property('url');
+            wss.url.should.be.a('object');
+            wss.url.should.have.property('protocol', 'ws:');
+            wss.url.should.have.property('hostname', 'localhost');
+            wss.url.should.have.property('href', ressource);
+            wss.url.should.have.property('host', 'localhost:3000');
+            wss.url.should.have.property('port', '3000');
+        });
+    });
+
     describe('#listen()', function() {
         it('should listen on the defined url for upgrade requests', function(done) {
             var wss = new WebSocketServer({ url: ressource });
