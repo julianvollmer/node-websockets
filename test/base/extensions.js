79850859043dfb7b02dee697aa77e271e9a1905a
bodokaiser <bodo.kaiser@enabre.com>
2013-02-18 19:28:41 +0100

    beginning new extensions test

---
+++ test/base/extensions.js
@@ -0,0 +1,83 @@
+var util = require('util');
+var should = require('should');
+
+var WebSocketBase = require('../../lib/base');
+
+describe('WebSocketBase', function() {
+    
+    var wsb;
+    var str = 'x-test-ext';
+    var func = function() {};
+
+    beforeEach(function() {
+        wsb = new WebSocketBase();
+    });
+
+    describe('extensions', function() {
+        it('should be an empty array as default', function() {
+            wsb.extensions.should.be.empty;
+            wsb.extensions.should.be.an.instanceOf(Array);
+        });
+    });
+
+    describe('#addExtension(name, read, write)', function() {
+        it('should be a function', function() {
+            wsb.addExtension.should.be.a('function');    
+        });
+        
+        it('should throw error on wrong arguments', function() {
+            // read not fn
+            (function() {
+                wsb.addExtension(str, str, func);
+            }).should.throwError();
+            // write not fn
+            (function() {
+                wsb.addExtension(str, func, str);
+            }).should.throwError();
+            // name not str
+            (function() {
+                wsb.addExtension(func, func, func);
+            }).should.throwError();
+        });
+        
+        it('should not throw error on right arguments', function() {
+            (function() {
+                wsb.addExtension(str, func, func);
+            }).should.not.throwError();
+        });
+        
+        it('should return reference of this', function() {
+            wsb.addExtension(str, func, func).should.equal(wsb);
+        });
+        
+        it('should add an extension object to internal extensions array', function() {
+            wsb.addExtension(str, func, func);
+            // check array
+            wsb.extensions.should.not.be.empty;
+            wsb.extensions.should.have.length(1);
+            // check array item
+            wsb.extensions[0].should.be.a('object');
+            wsb.extensions[0].should.have.property('name', str);
+            wsb.extensions[0].should.have.property('read', func);
+            wsb.extensions[0].should.have.property('write', func);
+            wsb.extensions[0].should.have.property('enabled', false);
+        });
+    });
+
+    describe('#hasExtension(name)', function() {
+
+    });
+
+    describe('#removeExtension(name)', function() {
+
+    });
+
+    describe('#enableExtensions(extensions)', function() {
+
+    });
+
+    describe('#disableExtensions(extensions)', function() {
+
+    });
+
+});
