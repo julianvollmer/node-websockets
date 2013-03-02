f9524cb7a1d45b3a2f59b6e368044d02b9b64dcc
bodokaiser <bodo.kaiser@enabre.com>
2013-03-02 22:27:28 +0100

    added test for covering WebSocketBase #send([sid,] data) with extensions

--- test/base/send_with_extensions.js
+++ test/base/send_with_extensions.js
@@ -1,45 +1,51 @@
 var should = require('should');
 
 var MockupSocket = require('../mockup/socket');
+var mockupExtensions = require('../mockup/extensions');
+
 var WebSocketBase = require('../../lib/base');
 var WebSocketFrame = require('../../lib/frame');
 
 describe('WebSocketBase', function() {
     
-    var str, wsb, socketOne, socketTwo;
+    var str, wsb, options, socketOne, socketTwo;
 
     beforeEach(function() {
         str = 'Hello World.';
+
         wsb = new WebSocketBase();
         socketOne = new MockupSocket();
         socketTwo = new MockupSocket();
 
-        wsb.assignSocket(socketOne);
-        wsb.assignSocket(socketTwo);
+        options = { extensions: Object.keys(mockupExtensions) };
+
+        wsb.extensions = mockupExtensions;
+        wsb.assignSocket(socketOne, options);
+        wsb.assignSocket(socketTwo, options);
     });
 
-    describe('#send(data)', function() {
+    describe('#send(data) with two extensions', function() {
         it('should send a text frame to socket one and two', function(done) {
             socketOne.once('data', function(chunk) {
                 var frame = new WebSocketFrame(chunk);
                 frame.fin.should.be.true;
                 frame.opcode.should.equal(0x01);
-                frame.length.should.equal(0x0c);
-                frame.content.toString().should.equal(str);
+                frame.length.should.equal(0x0c + 8);
+                frame.content.toString().should.equal(str + 'bubutaja');
             });
             socketTwo.once('data', function(chunk) {
                 var frame = new WebSocketFrame(chunk);
                 frame.fin.should.be.true;
                 frame.opcode.should.equal(0x01);
-                frame.length.should.equal(0x0c);
-                frame.content.toString().should.equal(str);
+                frame.length.should.equal(0x0c + 8);
+                frame.content.toString().should.equal(str + 'bubutaja');
                 done();
             });
             wsb.send(str);
         });
     });
 
-    describe('#send(sid, data)', function(done) {
+    describe('#send(sid, data) with two extensions', function(done) {
         it('should only send a text frame to socket two', function(done) {
             socketOne.once('data', function(chunk) {
                 throw new should.AssertionError('only socketTwo should receive data');
@@ -48,8 +54,8 @@ describe('WebSocketBase', function() {
                 var frame = new WebSocketFrame(chunk);
                 frame.fin.should.be.true;
                 frame.opcode.should.equal(0x01);
-                frame.length.should.equal(0x0c);
-                frame.content.toString().should.equal(str);
+                frame.length.should.equal(0x0c + 8);
+                frame.content.toString().should.equal(str + 'bubutaja');
                 done();
             });
             wsb.send(1, str);
@@ -57,3 +63,4 @@ describe('WebSocketBase', function() {
     });
 
 });
+
