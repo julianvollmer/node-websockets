3f130ea122db6160248bc19933d7c4504c4a3076
bodokaiser <bodo.kaiser@enabre.com>
2013-03-04 16:30:00 +0100

    added test for providing sids as array in WebSocketBase #send

--- test/base/send.js
+++ test/base/send.js
@@ -59,4 +59,39 @@ describe('WebSocketBase', function() {
         });
     });
 
+    describe('#send(sids, data)', function(done) {
+        it('should only send a text frame to socket two', function(done) {
+            socketOne.once('data', function(chunk) {
+                throw new should.AssertionError('only socketTwo should receive data');
+            });
+            socketTwo.once('data', function(chunk) {
+                var frame = new WebSocketFrame(chunk);
+                frame.fin.should.be.true;
+                frame.opcode.should.equal(0x01);
+                frame.length.should.equal(0x0c);
+                frame.content.toString().should.equal(str);
+                done();
+            });
+            wsb.send([socketTwoId], str);
+        });
+        it('should send a text frame to socket one and two', function(done) {
+            socketOne.once('data', function(chunk) {
+                var frame = new WebSocketFrame(chunk);
+                frame.fin.should.be.true;
+                frame.opcode.should.equal(0x01);
+                frame.length.should.equal(0x0c);
+                frame.content.toString().should.equal(str);
+            });
+            socketTwo.once('data', function(chunk) {
+                var frame = new WebSocketFrame(chunk);
+                frame.fin.should.be.true;
+                frame.opcode.should.equal(0x01);
+                frame.length.should.equal(0x0c);
+                frame.content.toString().should.equal(str);
+                done();
+            });
+            wsb.send([socketOneId, socketTwoId], str);
+        });
+    });
+
 });
