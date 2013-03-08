33e872f5b222447b584d0aa18e6269ac76efde74
bodokaiser <bodo.kaiser@enabre.com>
2013-03-08 10:02:48 +0100

    solved issue #26 and enabled close event tests

--- test/base/events.js
+++ test/base/events.js
@@ -2,6 +2,8 @@ var util = require('util');
 var should = require('should');
 
 var MockupSocket = require('../mockup/socket');
+var mockupFrames = require('../mockup/frames');
+
 var WebSocketBase = require('../../lib/base');
 
 describe('WebSocketBase', function() {
@@ -12,8 +14,6 @@ describe('WebSocketBase', function() {
     beforeEach(function() {
         wsb = new WebSocketBase();
         socket = new MockupSocket();
-
-        wsb.assignSocket(socket);
     });
 
     describe('event: "open"', function() {
@@ -32,21 +32,50 @@ describe('WebSocketBase', function() {
                 
                 done(); 
             });
+            wsb.assignSocket(socket);
             wsb.ping('ping-pong');
         });
     });
     
-    xdescribe('event: "close"', function() {
- 
+    describe('event: "close"', function() {
+        it('should emit a close event when connection is closed', function(done) {    
+            wsb.once('open', function(reason, sid) {
+                wsb.close(reason, sid);
+            });
+            wsb.once('close', function() {
+                done();
+            });
+            wsb.assignSocket(socket);
+        });
+        it('should emit a close event when a close frame is received', function(done) {
+            wsb.once('close', function() {
+                done();
+            });
+            wsb.assignSocket(socket);
+            socket.write(mockupFrames.maskedCloseFrame.frame);
+        });
     });
     
+    describe('event: "custom"', function() {
+        it('should emit a real event when listening on the sockets custom event', function(done) {
+            wsb.once('open', function(sid) {
+                wsb.sockets[sid].emit('custom', 'heartbeat', true);
+            });
+            wsb.once('heartbeat', function(someVal) {
+                someVal.should.be.true;
+                done();
+            });
+            wsb.assignSocket(socket);
+        });
+    });
+
     describe('event: "message"', function() {
         it('should be a message event emitted when getting a data frame', function(done) {
             wsb.once('message', function(message) {
                 message.should.equal('nodejs is fucking great');
-                
                 done();
             });
+            wsb.assignSocket(socket);
             wsb.send('nodejs is fucking great');
         });
     });
