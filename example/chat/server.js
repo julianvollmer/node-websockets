3f339974f11e0890d96c1ed0fade0a673fccbd44
bodokaiser <bodo.kaiser@enabre.com>
2013-03-25 18:23:54 +0100

    fixed good known bug with unmasking the content when payload was concated

--- example/chat/server.js
+++ example/chat/server.js
@@ -11,6 +11,29 @@ var httpServer = http.createServer();
 var imageSocketServer = new websockets.Server({ url: "ws://localhost:3000/images", timeout: 0 });
 var messageSocketServer = new websockets.Server({ url: "ws://localhost:3000/messages", timeout: 0 });
 
+imageSocketServer.on('message', function(message, wssocket) {
+    imageSocketServer.broadcast(message);
+});
+
+// informs about new clients connected
+messageSocketServer.on('open', function(wssocket) {
+    messageSocketServer.broadcast(util.format('Client #%d has connected', wssocket.index));
+});
+
+// informs about clients left
+messageSocketServer.on('close', function(reason, wssocket) {
+    messageSocketServer.broadcast(util.format('Client #%d has left', wssocket.index));
+});
+
+// shares a sent message through all clients
+messageSocketServer.on('message', function(message, wsocket) {
+    console.log('message:', message);
+    messageSocketServer.broadcast(message);
+});
+
+imageSocketServer.listen(httpServer);
+messageSocketServer.listen(httpServer);
+
 // server static files
 httpServer.on('request', function(req, res) {
     var urll = url.parse(req.url);
@@ -40,28 +63,5 @@ httpServer.on('request', function(req, res) {
         });
     });
 });
-
-imageSocketServer.on('message', function(message, wssocket) {
-    imageSocketServer.broadcast(message);
-});
-
-// informs about new clients connected
-messageSocketServer.on('open', function(wssocket) {
-    messageSocketServer.broadcast(util.format('Client #%d has connected', wssocket.index));
-});
-
-// informs about clients left
-messageSocketServer.on('close', function(reason, wssocket) {
-    messageSocketServer.broadcast(util.format('Client #%d has left', wssocket.index));
-});
-
-// shares a sent message through all clients
-messageSocketServer.on('message', function(message, wsocket) {
-    console.log('message:', message);
-    messageSocketServer.broadcast(message);
-});
-
-imageSocketServer.listen(httpServer);
-messageSocketServer.listen(httpServer);
 
 httpServer.listen(3000);
