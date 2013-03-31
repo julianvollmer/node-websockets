3771b8701c67b9a21754ae78c37e684c28a03e18
bodokaiser <bodo.kaiser@enabre.com>
2013-03-31 21:35:49 +0200

    implemented ability to write frames
    You can now write your payload directly to an instance of WebSocketStream.
    The head information are partly extracted from the payload (e.g. length) but
    also from the flags defined in "wsstream.writeFrameState" like fin, mask, etc.
    Currently we are missing body write tests (head tests are available).

---
+++ test/stream/write_head.js
@@ -0,0 +1,241 @@
+var stream = require('stream');
+
+var WebSocketStream = require('../../lib/stream');
+
+describe('WebSocketStream', function() {
+
+    var msocket, wsstream;
+
+    beforeEach(function() {
+        msocket = new stream.Duplex();
+        msocket._read = function() {};
+        msocket._write = function(chunk, encoding, done) {
+            this.emit('data', chunk);
+
+            done(null);
+        };
+        wsstream = new WebSocketStream(msocket);
+    });
+
+    describe('writing head bytes', function() {
+
+        it('should set fin bit', function(done) {
+            msocket.once('data', function(chunk) {
+                var fin = chunk[0] & 0x80;
+                
+                fin.should.equal(0x80);
+                chunk.length.should.equal(2);
+                
+                done();
+            });
+            wsstream.writeFrameState.fin = true;
+            wsstream.write(new Buffer(0));
+        });
+
+        it('should not set fin bit', function(done) {
+            msocket.once('data', function(chunk) {
+                var fin = chunk[0] & 0x80;
+                
+                fin.should.equal(0);
+                chunk.length.should.equal(2);
+                
+                done();
+            });
+            wsstream.writeFrameState.fin = false;
+            wsstream.write(new Buffer(0));
+        });
+
+        it('should set mask bit', function(done) {
+            msocket.once('data', function(chunk) {
+                var mask = chunk[1] & 0x80;
+                
+                mask.should.equal(0x80);
+                chunk.length.should.equal(6);
+                
+                done();
+            });
+            wsstream.writeFrameState.mask = true;
+            wsstream.write(new Buffer(0));
+        });
+
+        it('should not set mask bit', function(done) {
+            msocket.once('data', function(chunk) {
+                var mask = chunk[1] & 0x80;
+
+                mask.should.equal(0);
+                chunk.length.should.equal(2);
+                
+                done();
+            });
+            wsstream.writeFrameState.mask = false;
+            wsstream.write(new Buffer(0));
+        });
+
+        it('should set opcode to 0x00', function(done) {
+            msocket.once('data', function(chunk) {
+                var opcode = chunk[0] & 0x0f;
+                
+                opcode.should.equal(0x00);
+                chunk.length.should.equal(2);
+                
+                done();
+            });
+            wsstream.writeFrameState.opcode = 0x00;
+            wsstream.write(new Buffer(0));
+        });
+
+        it('should set opcode 0x0a', function(done) {
+            msocket.once('data', function(chunk) {
+                var opcode = chunk[0] & 0x0f;
+                
+                opcode.should.equal(0x0a);
+                chunk.length.should.equal(2);
+                
+                done();
+            });
+            wsstream.writeFrameState.opcode = 0x0a;
+            wsstream.write(new Buffer(0));
+        });
+    
+        it('should set length to 0', function(done) {
+            msocket.once('data', function(chunk) {
+                var length = chunk[1] & 0x7f;
+                
+                length.should.equal(0);
+                chunk.length.should.equal(2);
+                
+                done();
+            });
+            wsstream.write(new Buffer(0));
+        });
+    
+        it('should set length to 125', function(done) {
+            msocket.once('data', function(chunk) {
+                var length = chunk[1] & 0x7f;
+                
+                length.should.equal(125);
+                chunk.length.should.equal(2);
+                
+                done();
+            });
+            wsstream.write(new Buffer(125));
+        });
+
+        it('should set length to 126', function(done) {
+            msocket.once('data', function(chunk) {
+                var hlength = chunk[1] & 0x7f;
+                var plength = chunk.readUInt16BE(2);
+
+                hlength.should.equal(126);
+                plength.should.equal(126);
+                chunk.length.should.equal(4);
+                
+                done();
+            });
+            wsstream.write(new Buffer(126));
+        });
+    
+        it('should set length to 127', function(done) {
+            msocket.once('data', function(chunk) {
+                var hlength = chunk[1] & 0x7f;
+                var plength = chunk.readUInt16BE(2);
+
+                hlength.should.equal(126);
+                plength.should.equal(127);
+                chunk.length.should.equal(4);
+                
+                done();
+            });
+            wsstream.write(new Buffer(127));
+        });
+        
+        it('should set length to 65535', function(done) {
+            msocket.once('data', function(chunk) {
+                var hlength = chunk[1] & 0x7f;
+                var plength = chunk.readUInt16BE(2);
+
+                hlength.should.equal(126);
+                plength.should.equal(65535);
+                chunk.length.should.equal(4);
+                
+                done();
+            });
+            wsstream.write(new Buffer(65535));
+        });
+    
+        it('should set length to 65536', function(done) {
+            msocket.once('data', function(chunk) {
+                var hlength = chunk[1] & 0x7f;
+                var plength = chunk.readUInt32BE(6);
+
+                hlength.should.equal(127);
+                plength.should.equal(65536);
+                chunk.length.should.equal(10);
+                
+                done();
+            });
+            wsstream.write(new Buffer(65536));
+        });
+
+        // exceeds v8 object limit (the buffer)
+        // TODO: throw error check
+        xit('should set length to 4294967295', function(done) {
+            msocket.once('data', function(chunk) {
+                var hlength = chunk[1] & 0x7f;
+                var plength = chunk.readUInt32BE(6);
+
+                hlength.should.equal(127);
+                plength.should.equal(4294967295);
+                chunk.length.should.equal(10);
+                
+                done();
+            });
+            wsstream.write(new Buffer(4294967295));
+        });
+
+        it('should have masking bytes, length > 126', function(done) {
+            msocket.once('data', function(chunk) {
+                var length = chunk[1] & 0x7f;
+
+                length.should.equal(125);
+                chunk.length.should.equal(6);
+                
+                done();
+            });
+            wsstream.writeFrameState.mask = true;
+            wsstream.write(new Buffer(125));
+        });
+
+        it('should have masking bytes, length > 65536', function(done) {
+            msocket.once('data', function(chunk) {
+                var hlength = chunk[1] & 0x7f;
+                var plength = chunk.readUInt16BE(2);
+
+                hlength.should.equal(126);
+                plength.should.equal(65535);
+                chunk.length.should.equal(8);
+                
+                done();
+            });
+            wsstream.writeFrameState.mask = true;
+            wsstream.write(new Buffer(65535));
+        });
+
+        it('should have masking bytes, length > 65537', function(done) {
+            msocket.once('data', function(chunk) {
+                var hlength = chunk[1] & 0x7f;
+                var plength = chunk.readUInt32BE(6);
+
+                hlength.should.equal(127);
+                plength.should.equal(65536);
+                chunk.length.should.equal(14);
+                
+                done();
+            });
+            wsstream.writeFrameState.mask = true;
+            wsstream.write(new Buffer(65536));
+        });
+
+    });
+
+});
