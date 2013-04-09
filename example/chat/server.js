a480209890453fba649cca3ba462a6524fbcd751
bodokaiser <bodo.kaiser@enabre.com>
2013-04-09 08:50:31 +0200

    rebuilding writting part of node-websockets
    I mentioned that the WebSocketServer does not send the final frame on end of
    piping. At least not to all clients (only to the uploading one...). In order to
    fix this properly we must refactor a lot. For example we have the
    WebSocketOutgoing we do not use. All the refactoring on the write part will be
    happen in this pull request.

--- example/chat/server.js
+++ example/chat/server.js
@@ -20,6 +20,7 @@ imageSocketServer.on('stream:start', function(wssocket) {
 
 imageSocketServer.on('stream:end', function(wssocket) {
     console.log('ending stream');
+    wssocket.write(new Buffer(0));
     wssocket.unpipe(imageSocketServer);
 });
 
