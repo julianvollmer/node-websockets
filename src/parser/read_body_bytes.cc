e2a182a250d31c9a0080da2185692031015439dc
bodokaiser <bodo.kaiser@enabre.com>
2013-04-14 11:34:05 +0200

    completed port of #readBodyBytes

--- src/parser/read_body_bytes.cc
+++ src/parser/read_body_bytes.cc
@@ -38,13 +38,55 @@ Handle<Value> ReadBodyBytes(const Arguments &args) {
         return scope.Close(Undefined());
     }
 
+    /* copy state.mask to boolean */
+    bool mask = state->Get(String::New("mask"))
+        ->BooleanValue();
+
+    /* copy state.index to integer */
     int index = state->Get(String::New("index"))
         ->NumberValue();
     
-    int length = state->Get(String::New("length"))
+    /* copy state.length to integer */
+    int plength = state->Get(String::New("length"))
         ->NumberValue();
 
-    byte* buf = (byte*) node::Buffer::Data(chunk);
+    /* copy chunk.length to integer */
+    int clength = node::Buffer::Length(chunk);
 
-    return scope.Close(Undefined());
+    /* says us how much we need to read from chunk */
+    int toRead = plength - index;
+
+    if (toRead > clength)
+        toRead = clength;
+
+    /* copy buffer data to unsigned char array */
+    byte* bodyChunk = (byte*) node::Buffer::Data(chunk);
+
+    /* if we already have read enough return an empty buffer */
+    if (toRead == 0)
+        return scope.Close(node::Buffer::New(0)->handle_);
+
+    /* else it can be that chunk is bigger than required */
+    byte* body = (byte*) malloc(toRead);
+
+    /* so we copy only the required item pointers */
+    for (int i = 0; i < toRead; i++)
+        body[i] = bodyChunk[i];
+
+    /* free memory of body chunk because we do not require it anymore */
+    /*free(bodyChunk);*/
+
+    if (mask) {
+        byte* masking = (byte*) node::Buffer::Data(state->Get(String::New("masking"))
+                ->ToObject());
+
+        for (int i = 0; i < toRead; i++)
+            body[i] = body[i] ^ masking[(i + index) % 4];
+    }
+
+    index += toRead;
+
+    state->Set(String::New("index"), Number::New(index));
+
+    return scope.Close(node::Buffer::New((const char*)body, toRead)->handle_);
 }
--- test/native-parser/read_body_bytes.js
+++ test/native-parser/read_body_bytes.js
@@ -17,22 +17,28 @@ describe('native WebSocketParser', function() {
             parser.readBodyBytes(state, chunk);
         });
 
-        xit('should return chunk', function() {
+        it('should return chunk', function() {
             chunk = new Buffer('Hello');
+            state.length = 5;
             parser.readBodyBytes(state, chunk).should.eql(chunk);
             state.index.should.equal(5);
         });
 
-        xit('should return masked body', function() {
+        it('should return masked body', function() {
             state.mask = true;
+            state.length = 5;
             state.masking = new Buffer([0x23, 0x7b, 0xfa, 0xc2]);
             var chunks = [];
             var chunkOne = new Buffer([0x6b, 0x1e]);
             var chunkTwo = new Buffer([0x96, 0xae, 0x4c]);
+
             chunks.push(parser.readBodyBytes(state, chunkOne));
+            state.index.should.equal(2);
+
             chunks.push(parser.readBodyBytes(state, chunkTwo));
-            Buffer.concat(chunks).should.eql(new Buffer('Hello'));
             state.index.should.equal(5);
+            
+            Buffer.concat(chunks).should.eql(new Buffer('Hello'));
         });
 
         it('should throw error if first argument not an object', function() {
